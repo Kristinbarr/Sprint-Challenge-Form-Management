c57440a77567260248e239b74a9a77d81370aed5
Kristin Barr <kristinbarr@gmail.com>
2019-08-09 12:07:43 -0400

    form validation touched feedback

--- BE/client/src/components/UserForm.js
+++ BE/client/src/components/UserForm.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Field, withFormik } from 'formik'
 import * as Yup from 'yup'
 
 const UserForm = ({ errors, touched, status }) => {
+  // const [users, setUsers] = useState([])
+
   return (
     <Form>
       <div>
         <Field type='text' name='username' placeholder='Name' />
+        {touched.username && errors.username && (
+          <p className='error'>{errors.username}</p>
+        )}
       </div>
       <div>
         <Field type='password' name='password' placeholder='Password' />
+        {touched.password && errors.password && (
+          <p className='error'>{errors.password}</p>
+        )}
       </div>
+      <button>SUBMIT</button>
     </Form>
   )
 }
